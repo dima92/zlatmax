73df00d87fc23df9061e728f43cc8ed8933b9802
dima92 <dima-tkachenko@mail.ru>
2022-03-03 17:17:46 +0300

    feat: feat admin navigation and routes

--- Shop/src/app/app.module.ts
+++ Shop/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
+
 import {AppComponent} from './app.component';
 import {MainLayoutComponent} from './shared/main-layout/main-layout.component';
 import {MainPageComponent} from './main-page/main-page.component';
